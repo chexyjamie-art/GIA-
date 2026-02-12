aeb456578271bc16727140162a94515e2a4f26f4
chexyjamie-art <chexyjamie@gmail.com>
2026-02-12 08:51:36 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#6A8DFF",
+        secondary:"#9B59FF",
+        glass:"rgba(255,255,255,0.7)"
+      }
+    },
+  },
+  plugins: [],
+}
