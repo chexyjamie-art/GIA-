e5f5820a1e4fb75dd87882ca8cd9308a5d5dbff3
chexyjamie-art <chexyjamie@gmail.com>
2026-02-12 10:43:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,22 @@ function App(){
 }
 
 export default App
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import ProductDetail from "./pages/ProductDetail"
+
+function App(){
+  return(
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
