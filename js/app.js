15e0e5a2058cc34b47317419543f291b6c85e1dc
chexyjamie-art <chexyjamie@gmail.com>
2026-02-12 08:17:11 +0530

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Basic placeholder JS (future AI features add karenge)
-
-const search = document.querySelector(".search");
-
-search.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    alert("Searching: " + search.value);
-  }
-});
