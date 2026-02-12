76506f45483e1fbcc2e4f8aac1f4a84ff8d56f14
chexyjamie-art <chexyjamie@gmail.com>
2026-02-12 10:48:16 +0530

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -21,3 +21,23 @@ export default function ProductCard({p}){
     </div>
   )
 }
+
+import { Link } from "react-router-dom"
+
+<Link to={`/product/${p.id}`}>
+<div className="bg-white w-[260px] rounded-2xl shadow-md p-4 hover:shadow-xl transition cursor-pointer">
+
+<img src={p.img} className="rounded-xl mb-3"/>
+
+<h3 className="font-semibold text-lg">{p.name}</h3>
+
+<p className="text-gray-500 text-sm mb-2">
+₹{p.price_min} - ₹{p.price_max}
+</p>
+
+<div className="text-yellow-400 mb-3">
+{"★".repeat(p.rating)}
+</div>
+
+</div>
+</Link>
