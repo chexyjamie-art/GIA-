6c04c12b09288179aeec749a23123f2c948db0f4
chexyjamie-art <chexyjamie@gmail.com>
2026-02-21 14:30:02 +0530

    Delete affiliate.js

--- server/affiliate.js
+++
@@ -1,5 +0,0 @@
-export const makeAffiliate=name=>{
- if(name.toLowerCase().includes("iphone"))
-  return "https://amzn.to/yourid"
- return "https://affiliate-link.com?q="+encodeURIComponent(name)
-}
