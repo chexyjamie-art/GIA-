ec21023b11c2e33b724554d6cfb57d16294e97ca
chexyjamie-art <chexyjamie@gmail.com>
2026-02-21 14:30:39 +0530

    Delete compare.js

--- server/compare.js
+++
@@ -1,7 +0,0 @@
-export function compareProducts(a,b){
- let A=0,B=0
- if(a.price<b.price)A++; else B++
- if(a.rating>b.rating)A++; else B++
- if(a.reviews>b.reviews)A++; else B++
- return A>B?a:b
-}
